3c8cfa95b4ac872d22ed628b4929a2c3840a0de4
Max Andreacchi <84281259+atomicchonk@users.noreply.github.com>
2026-03-25 12:46:56 -0400

    Add HelloWorld.sh

---
+++ HelloWorld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOSTNAME=$(hostname)
+
+LOCAL_IP=$(ipconfig getifaddr en0 2>/dev/null)
+
+if [ -z "$LOCAL_IP" ]; then
+  LOCAL_IP=$(ipconfig getifaddr en1 2>/dev/null)
+fi
+
+echo "Hostname: $HOSTNAME"
+echo "Local IP: $LOCAL_IP"
