4743546e1db03ca0c3b1b1437ed5bf7ce64358cc
Max Andreacchi <84281259+atomicchonk@users.noreply.github.com>
2026-03-25 15:37:56 -0400

    Fix syntax error in WebEx.sh

--- WebEx.sh
+++ WebEx.sh
@@ -9,7 +9,7 @@ if [ -z "$LOCAL_IP" ]; then
   LOCAL_IP=$(ipconfig getifaddr en1 2>/dev/null)
 fi
 
-if [ -f "webex-installer.sh ]; then
+if [ -f "webex-installer.sh" ]; then
   echo "Hostname: $HOSTNAME"
   echo "Local IP: $LOCAL_IP"
 fi
